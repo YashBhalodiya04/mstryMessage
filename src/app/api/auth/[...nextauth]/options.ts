6aec4c3273bc671447b7d16047a0fc2adbf32515
Yash <yashbhalodia2002@gmail.com>
2024-10-20 14:02:46 +0530

    options change

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,72 +1,73 @@
-import { NextAuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import bcrypt from "bcryptjs";
-import dbconnect from "@/lib/dbConect";
-import UserModal from "@/model/User";
+import { NextAuthOptions } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import bcrypt from 'bcryptjs';
+import dbConnect from '@/lib/dbConect';
+import UserModel from '@/model/User';
 
 export const authOptions: NextAuthOptions = {
   providers: [
     CredentialsProvider({
-      name: "Credentials",
-      id: "Credentials",
+      id: 'credentials',
+      name: 'Credentials',
       credentials: {
-        email: { label: "Email", type: "text" },
-        password: { label: "Password", type: "password" },
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' },
       },
       async authorize(credentials: any): Promise<any> {
-        await dbconnect();
+        await dbConnect();
         try {
-          const user = await UserModal.findOne({
+          const user = await UserModel.findOne({
             $or: [
               { email: credentials.identifier },
-              { userName: credentials.identifier },
+              { username: credentials.identifier },
             ],
           });
           if (!user) {
-            throw new Error("Invalid credentials");
+            throw new Error('No user found with this email');
           }
           if (!user.isVarified) {
-            throw new Error("User is not varified");
+            throw new Error('Please verify your account before logging in');
           }
           const isPasswordCorrect = await bcrypt.compare(
             credentials.password,
             user.password
           );
-          if (!isPasswordCorrect) {
-            throw new Error("Invalid credentials");
+          if (isPasswordCorrect) {
+            return user;
+          } else {
+            throw new Error('Incorrect password');
           }
-          return user;
-        } catch (error: any) {
-          throw new Error(error);
+        } catch (err: any) {
+          throw new Error(err);
         }
       },
     }),
   ],
-  pages: {
-    signIn: "/signin",
-  },
-  session: {
-    strategy: "jwt",
-  },
-  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     async jwt({ token, user }) {
       if (user) {
-        token._id = user._id?.toString();
+        token._id = user._id?.toString(); // Convert ObjectId to string
         token.isVerified = user.isVarified;
         token.isAcceptingMessages = user.isAcceptingMessages;
-        token.userName = user.userName;
+        token.username = user.userName;
       }
       return token;
     },
     async session({ session, token }) {
-        if (token) {
+      if (token) {
         session.user._id = token._id;
-        session.user.isVarified = token.isVerified;
+        session.user.isVerified = token.isVerified;
         session.user.isAcceptingMessages = token.isAcceptingMessages;
-        session.user.userName = token.userName;
+        session.user.username = token.username;
       }
       return session;
     },
+  },
+  session: {
+    strategy: 'jwt',
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: '/sign-in',
   },
 };
